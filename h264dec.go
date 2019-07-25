2d15ad8bafd62ff17b541f3ed8fd06b4ecb7ec6c
Yono Mittlefehldt <yono@toojuice.com>
2019-07-25 09:13:24 +0200

    Add AV prefix to defined constant

--- h264dec.go
+++ h264dec.go
@@ -15,7 +15,7 @@ import (
 	} h264dec_t ;
 
 	static int h264dec_new(h264dec_t *h, uint8_t *data, int len) {
-		h->c = avcodec_find_decoder(CODEC_ID_H264);
+		h->c = avcodec_find_decoder(AV_CODEC_ID_H264);
 		h->ctx = avcodec_alloc_context3(h->c);
 		h->f = avcodec_alloc_frame();
 		h->ctx->extradata = data;
